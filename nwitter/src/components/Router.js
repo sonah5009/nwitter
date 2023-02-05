61b2d62eb4a8830d60461af664f93f1a5222e128
sonah5009 <sonah5009@gmail.com>
2023-02-06 00:00:27 +0900

    Upload: App.js

--- nwitter/src/components/Router.js
+++ nwitter/src/components/Router.js
@@ -4,8 +4,7 @@ import { useState } from "react";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
 
-const AppRouter = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+const AppRouter = ({isLoggedIn}) => {
   return(
     <BrowserRouter>
       <Routes>
